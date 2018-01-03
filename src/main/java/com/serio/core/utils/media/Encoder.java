829475967ea5ec64041ce0f82d13510b4bd223b4
serio.shi <lionsmail1015@gmail.com>
2018-01-03 15:59:08 +0800

    Amend encoder for process annotation

--- src/main/java/com/serio/core/utils/media/Encoder.java
+++ src/main/java/com/serio/core/utils/media/Encoder.java
@@ -720,235 +720,7 @@ public class Encoder {
 		encode(source, target, attributes, null);
 	}
 
-	/**
-	 * Re-encode a multimedia file.
-	 * 
-	 * @param source
-	 *            The source multimedia file. It cannot be null. Be sure this
-	 *            file can be decoded (see
-	 *            {@link Encoder#getSupportedDecodingFormats()},
-	 *            {@link Encoder#getAudioDecoders()} and
-	 *            {@link Encoder#getVideoDecoders()}).
-	 * @param target
-	 *            The target multimedia re-encoded file. It cannot be null. If
-	 *            this file already exists, it will be overwrited.
-	 * @param attributes
-	 *            A set of attributes for the encoding process.
-	 * @param listener
-	 *            An optional progress listener for the encoding process. It can
-	 *            be null.
-	 * @throws IllegalArgumentException
-	 *             If both audio and video parameters are null.
-	 * @throws InputFormatException
-	 *             If the source multimedia file cannot be decoded.
-	 * @throws EncoderException
-	 *             If a problems occurs during the encoding process.
-	 */
-/*	public void encode(File source, File target, EncodingAttributes attributes,
-			EncoderProgressListener listener) throws IllegalArgumentException,
-			InputFormatException, EncoderException {
-		
-		String formatAttribute = attributes.getFormat();
-		Float offsetAttribute = attributes.getOffset();
-		Float durationAttribute = attributes.getDuration();
-		
-		AudioAttributes audioAttributes = attributes.getAudioAttributes();
-		VideoAttributes videoAttributes = attributes.getVideoAttributes();
-		if (audioAttributes == null && videoAttributes == null) {
-			throw new IllegalArgumentException(
-					"Both audio and video attributes are null");
-		}
-		target = target.getAbsoluteFile();
-		target.getParentFile().mkdirs();
-		FFMPEGExecutor ffmpeg = locator.createExecutor();
-		if (offsetAttribute != null) {
-			ffmpeg.addArgument("-ss");
-			ffmpeg.addArgument(String.valueOf(offsetAttribute.floatValue()));
-		}
-		ffmpeg.addArgument("-i");
-		ffmpeg.addArgument(source.getAbsolutePath());
-		if (durationAttribute != null) {
-			ffmpeg.addArgument("-t");
-			ffmpeg.addArgument(String.valueOf(durationAttribute.floatValue()));
-		}
-		if (videoAttributes == null) {
-			ffmpeg.addArgument("-vn");
-		} else {
-			String codec = videoAttributes.getCodec();
-			if (codec != null) {
-				ffmpeg.addArgument("-vcodec");
-				ffmpeg.addArgument(codec);
-			}
-			String tag = videoAttributes.getTag();
-			if (tag != null) {
-				ffmpeg.addArgument("-vtag");
-				ffmpeg.addArgument(tag);
-			}
-			Integer bitRate = videoAttributes.getBitRate();
-			if (bitRate != null) {
-				ffmpeg.addArgument("-b");
-				ffmpeg.addArgument(String.valueOf(bitRate.intValue()));
-			}
-			Integer frameRate = videoAttributes.getFrameRate();
-			if (frameRate != null) {
-				ffmpeg.addArgument("-r");
-				ffmpeg.addArgument(String.valueOf(frameRate.intValue()));
-			}
-			VideoSize size = videoAttributes.getSize();
-			if (size != null) {
-				ffmpeg.addArgument("-s");
-				ffmpeg.addArgument(String.valueOf(size.getWidth()) + "x"
-						+ String.valueOf(size.getHeight()));
-			}
-			
-			Integer vFrame = videoAttributes.getVideoFrames();
-			if (vFrame != null) {
-				ffmpeg.addArgument("-vframes");
-				ffmpeg.addArgument(String.valueOf(vFrame.intValue()));
-			}
-		}
-		if (audioAttributes == null) {
-			ffmpeg.addArgument("-an");
-		} else {
-			String codec = audioAttributes.getCodec();
-			if (codec != null) {
-				ffmpeg.addArgument("-acodec");
-				ffmpeg.addArgument(codec);
-			}
-			Integer bitRate = audioAttributes.getBitRate();
-			if (bitRate != null) {
-				ffmpeg.addArgument("-ab");
-				ffmpeg.addArgument(String.valueOf(bitRate.intValue()));
-			}
-			Integer channels = audioAttributes.getChannels();
-			if (channels != null) {
-				ffmpeg.addArgument("-ac");
-				ffmpeg.addArgument(String.valueOf(channels.intValue()));
-			}
-			Integer samplingRate = audioAttributes.getSamplingRate();
-			if (samplingRate != null) {
-				ffmpeg.addArgument("-ar");
-				ffmpeg.addArgument(String.valueOf(samplingRate.intValue()));
-			}
-			Integer volume = audioAttributes.getVolume();
-			if (volume != null) {
-				ffmpeg.addArgument("-vol");
-				ffmpeg.addArgument(String.valueOf(volume.intValue()));
-			}
-		}
-		ffmpeg.addArgument("-f");
-		ffmpeg.addArgument(formatAttribute);
-		ffmpeg.addArgument("-y");
-		ffmpeg.addArgument(target.getAbsolutePath());
-		try {
-			ffmpeg.execute();
-		} catch (IOException e) {
-			throw new EncoderException(e);
-		}
-		try {
-			String lastWarning = null;
-			long duration;
-			long progress = 0;
-			RBufferedReader reader = null;
-			reader = new RBufferedReader(new InputStreamReader(ffmpeg
-					.getErrorStream()));
-			MultimediaInfo info = parseMultimediaInfo(source, reader);
-			if (durationAttribute != null) {
-				duration = (long) Math
-						.round((durationAttribute.floatValue() * 1000L));
-			} else {
-				duration = info.getDuration();
-				if (offsetAttribute != null) {
-					duration -= (long) Math
-							.round((offsetAttribute.floatValue() * 1000L));
-				}
-			}
-			if (listener != null) {
-				listener.sourceInfo(info);
-			}
-			int step = 0;
-			String line;
-			while ((line = reader.readLine()) != null) {
-				if (step == 0) {
-					if (line.startsWith("WARNING: ")) {
-						if (listener != null) {
-							listener.message(line);
-						}
-					} else if (!line.startsWith("Output #0")) {
-						throw new EncoderException(line);
-					} else {
-						step++;
-					}
-				} else if (step == 1) {
-					if (!line.startsWith("  ")) {
-						step++;
-					}
-				}
-				if (step == 2) {
-					if (!line.startsWith("Stream mapping:")) {
-						throw new EncoderException(line);
-					} else {
-						step++;
-					}
-				} else if (step == 3) {
-					if (!line.startsWith("  ")) {
-						step++;
-					}
-				}
-				if (step == 4) {
-					line = line.trim();
-					if (line.length() > 0) {
-						Hashtable table = parseProgressInfoLine(line);
-						if (table == null) {
-							if (listener != null) {
-								listener.message(line);
-							}
-							lastWarning = line;
-						} else {
-							if (listener != null) {
-								String time = (String) table.get("time");
-								if (time != null) {
-									int dot = time.indexOf('.');
-									if (dot > 0 && dot == time.length() - 2
-											&& duration > 0) {
-										String p1 = time.substring(0, dot);
-										String p2 = time.substring(dot + 1);
-										try {
-											long i1 = Long.parseLong(p1);
-											long i2 = Long.parseLong(p2);
-											progress = (i1 * 1000L)
-													+ (i2 * 100L);
-											int perm = (int) Math
-													.round((double) (progress * 1000L)
-															/ (double) duration);
-											if (perm > 1000) {
-												perm = 1000;
-											}
-											listener.progress(perm);
-										} catch (NumberFormatException e) {
-											;
-										}
-									}
-								}
-							}
-							lastWarning = null;
-						}
-					}
-				}
-			}
-			if (lastWarning != null) {
-				if (!SUCCESS_PATTERN.matcher(lastWarning).matches()) {
-					throw new EncoderException(lastWarning);
-				}
-			}
-		} catch (IOException e) {
-			throw new EncoderException(e);
-		} finally {
-			ffmpeg.destroy();
-		}
-	}*/
-	
-	
+
 	/**
 	 * Re-encode a multimedia file.
 	 * 
@@ -995,12 +767,13 @@ public class Encoder {
 			ffmpeg.addArgument(String.valueOf(offsetAttribute.floatValue()));
 		}
 		
-		ffmpeg.addArgument("-i");
-		ffmpeg.addArgument(source.getAbsolutePath());
 		if (durationAttribute != null) {
 			ffmpeg.addArgument("-t");
 			ffmpeg.addArgument(String.valueOf(durationAttribute.floatValue()));
 		}
+		
+		ffmpeg.addArgument("-i");
+		ffmpeg.addArgument(source.getAbsolutePath());
 		
 		setVideoAttriutes( ffmpeg, videoAttributes );
 		
@@ -1034,16 +807,19 @@ public class Encoder {
 			}
 			
 			Map<Field, Object> annotations = AnnotationProcesser.getAnnotationInfos( AudioAttributes.class, ArgName.class, "value" );
+			Field[] fields = AudioAttributes.class.getDeclaredFields();
 			
-			for ( Entry<Field, Object> entry : annotations.entrySet() ) {
-				Field  field = entry.getKey();
-				Object arg = ReflectionUtils.getObjectAttr(audioAttributes, field, audioAttributes.getClass());
-				Object value = entry.getValue();
-				
-				if (arg != null) {
-					ffmpeg.addArgument(String.valueOf(value));
-					ffmpeg.addArgument(String.valueOf(arg));
+			for ( Field field : fields ) {
+				if ( annotations.get(field) == null ) {
+					continue;
 				}
+				Object arg = ReflectionUtils.getObjectAttr(audioAttributes, field, audioAttributes.getClass());
+				if ( arg == null ) {
+					continue;
+				}
+				
+				ffmpeg.addArgument(String.valueOf(annotations.get(field)));
+				ffmpeg.addArgument(String.valueOf(arg));
 			}
 			
 		} catch (Exception e) {
@@ -1065,17 +841,22 @@ public class Encoder {
 			}
 			
 			Map<Field, Object> annotations = AnnotationProcesser.getAnnotationInfos( VideoAttributes.class, ArgName.class, "value" );
+			Field[] fields = VideoAttributes.class.getDeclaredFields();
 			
-			for ( Entry<Field, Object> entry : annotations.entrySet() ) {
-				Field  field = entry.getKey();
-				Object arg = ReflectionUtils.getObjectAttr(videoAttributes, field, videoAttributes.getClass());
-				Object value = entry.getValue();
-				
-				if (arg != null) {
-					ffmpeg.addArgument(String.valueOf(value));
-					ffmpeg.addArgument(String.valueOf(arg));
+			for ( Field field : fields ) {
+				if ( annotations.get(field) == null ) {
+					continue;
 				}
+				
+				Object arg = ReflectionUtils.getObjectAttr(videoAttributes, field, videoAttributes.getClass());
+				if ( arg == null ) {
+					continue;
+				}
+				
+				ffmpeg.addArgument(String.valueOf(annotations.get(field)));
+				ffmpeg.addArgument(String.valueOf(arg));
 			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
